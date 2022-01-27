7e3880786cdc60338b8b040f0f2a2cc34cc2f276
zOnlyKroks <finnrades@gmail.com>
2022-01-27 14:57:46 +0100

    Advanced Chemistry (fabric)

---
+++ src/main/java/de/zonlykroks/advancedchemistry/AdvancedChemistry.java
@@ -0,0 +1,11 @@
+package de.zonlykroks.advancedchemistry;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AdvancedChemistry implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/zonlykroks/advancedchemistry/client/AdvancedChemistryClient.java
@@ -0,0 +1,5 @@
+package de.zonlykroks.advancedchemistry.client;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class AdvancedChemistryClient {
+}
